8270c18262f822a84f6dd94a8d368ae8bf07a7b3
titanknis <titanknis@gmail.com>
2024-11-24 11:18:17 +0100

    Update before reinstall

--- nixos/boot.nix
+++ nixos/boot.nix
@@ -20,21 +20,24 @@
     allowDiscards = true;
   };
 
-  # Bootloader Configuration (GRUB)
-  boot.loader.grub.enable = true;
-  boot.loader.grub.device = "nodev"; # Install GRUB on the EFI system partition
-  boot.loader.grub.copyKernels = true; # Automatically copy kernels
-  boot.loader.grub.efiSupport = true; # Enable EFI support for GRUB
-  boot.loader.grub.enableCryptodisk = true; # Enable encrypted disk support for GRUB
+  # Bootloader Configuration (systemd)
+  boot.loader.systemd-boot.enable = true;
+  boot.loader.systemd-boot.canTouchEfiVariables = true;
 
   boot.loader.efi.efiSysMountPoint = "/boot"; # Mount point for EFI partition
   boot.loader.efi.canTouchEfiVariables = true; # Allow modification of EFI variables
+  # Bootloader Configuration (GRUB)
+  # boot.loader.grub.enable = true;
+  # boot.loader.grub.device = "nodev"; # Install GRUB on the EFI system partition
+  # boot.loader.grub.copyKernels = true; # Automatically copy kernels
+  # boot.loader.grub.efiSupport = true; # Enable EFI support for GRUB
+  # boot.loader.grub.enableCryptodisk = true; # Enable encrypted disk support for GRUB
 
   # Custom GRUB Entries for Reboot and Poweroff
-  boot.loader.grub.extraEntries = ''
-    menuentry "Reboot" { reboot }
-    menuentry "Poweroff" { halt }
-  '';
+  # boot.loader.grub.extraEntries = ''
+  #   menuentry "Reboot" { reboot }
+  #   menuentry "Poweroff" { halt }
+  # '';
 
   # Kernel Configuration (Using Zen Kernel)
   boot.kernelPackages = pkgs.linuxPackages_zen;
